190bc397ab4b4a185353f8c549e8de0735c878a7
AL <146753285+arnl2025@users.noreply.github.com>
2026-01-23 12:31:04 +0100

    Create products.js

---
+++ products.js
@@ -0,0 +1,11 @@
+const products = [
+  {
+    id: "nike-001",
+    name: "Nike Tech Fleece",
+    price: 520,
+    stock: 1,
+    image: "https://i.postimg.cc/zfDhD6wj/output-onlinepngtools.png",
+    qr: "https://i.postimg.cc/02dn/sish.png",
+    description: "Nike Tech Fleece"
+  }
+];
